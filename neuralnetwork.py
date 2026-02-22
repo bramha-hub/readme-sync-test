6932cf34d859f5d042bb9ba28578ea18342ec598
bramhaa_13 <105053129+bramha-hub@users.noreply.github.com>
2026-02-22 11:53:08 +0530

    Update and rename server.py to neuralnetwork.py

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -1,16 +1,3 @@
-from mcp.server.fastmcp import FastMCP
-# Create an MCP server
-mcp = FastMCP("Demo")
-
-print("Hello from server!")
-
-@mcp.tool()
-def usd_to_gbp(amount: float) -> float:
-    """Convert USD(dollars) to GBP(pounds sterling)"""
-    print("Hello from server!")
-    EXCHANGE_RATE = 0.79
-    return round (amount * EXCHANGE_RATE, 2)
-
 import torch
 import torch.nn as nn
 import torch.optim as optim
